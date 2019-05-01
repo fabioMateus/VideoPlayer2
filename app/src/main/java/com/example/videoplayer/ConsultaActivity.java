9dc7a874b05573146f14cbf6f1cec5bd621e4dc9
fabioMateus <fabio-mateus12@hotmail.com>
2019-05-01 12:22:20 +0100

    adicionadas uma atividade para cada um

--- app/src/main/java/com/example/videoplayer/ConsultaActivity.java
+++ app/src/main/java/com/example/videoplayer/ConsultaActivity.java
@@ -4,20 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class ConsultaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_consulta);
     }
-
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, ConsultaActivity.class);
+        Intent intent = new Intent(this, PlayerActivity.class);
         startActivity(intent);
     }
 }
