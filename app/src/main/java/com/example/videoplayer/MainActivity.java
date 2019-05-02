19924cc5f399ffe94352a6ac6ed818ddb9796e0e
fabioMateus <fabio-mateus12@hotmail.com>
2019-05-02 15:55:47 +0100

    Layout pronto já com img, nome, ano, duracao e categoria

--- app/src/main/java/com/example/videoplayer/MainActivity.java
+++ app/src/main/java/com/example/videoplayer/MainActivity.java
@@ -1,33 +1,29 @@
 package com.example.videoplayer;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
-import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 public class MainActivity extends AppCompatActivity {
 
-    int[] IMAGES = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7,
-            R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie4, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7};
+    int[] IMAGES = {R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7,
+            R.drawable.movie1, R.drawable.movie2, R.drawable.movie3, R.drawable.movie5, R.drawable.movie6, R.drawable.movie7};
 
-    String[] NAMES = {"Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5", "Movie 6", "Movie 7",
-            "Movie 8", "Movie 9", "Movie 10", "Movie 11", "Movie 12", "Movie 13", "Movie 14"};
+    String[] NAMES = {"Movie 1", "Movie 2", "Movie 3", "Movie 5", "Movie 6", "Movie 7",
+            "Movie 8", "Movie 9", "Movie 10", "Movie 12", "Movie 13", "Movie 14"};
 
-    String[] DESCRIPTIONS = {"Description BlaBláBlá 1", "Description BlaBláBlá 2", "Description BlaBláBlá 3", "Description BlaBláBlá 4", "Description BlaBláBlá 5",
-            "Description BlaBláBlá 6", "Description BlaBláBlá 7", "Description BlaBláBlá 8", "Description BlaBláBlá 9", "Description BlaBláBlá 10", "Description BlaBláBlá 11",
-            "Description BlaBláBlá 12", "Description BlaBláBlá 13", "Description BlaBláBlá 14"};
+    String[] YEARS = {"2015","2015","2015","2015","2015","2015","2015","2015","2015","2015","2015","2015"};
+
+    String[] DURATIONS = {"215","215","215","015","205","215","15","205","215","015","205","201"};
+
+    String[] CATEGORIES = {"Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy",
+            "Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy","Action, Comedy"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,11 +66,15 @@ public class MainActivity extends AppCompatActivity {
             convertView = getLayoutInflater().inflate(R.layout.elemento_listagem, null);
             ImageView imageView = (ImageView) convertView.findViewById(R.id.MovieImageView);
             TextView textViewName = (TextView) convertView.findViewById(R.id.MovieNameTextView);
-            TextView textViewDescription = (TextView) convertView.findViewById(R.id.MovieDescriptionTextView);
+            TextView textViewYear = (TextView) convertView.findViewById(R.id.MovieYearTextView);
+            TextView textViewCategorie = (TextView) convertView.findViewById(R.id.MovieCategorieTextView);
+            TextView textViewDuration = (TextView) convertView.findViewById(R.id.MovieDurationTextView);
 
             imageView.setImageResource(IMAGES[position]);
             textViewName.setText(NAMES[position]);
-            textViewDescription.setText(DESCRIPTIONS[position]);
+            textViewYear.setText(YEARS[position]);
+            textViewCategorie.setText(CATEGORIES[position]);
+            textViewDuration.setText(DURATIONS[position]+"min");
 
             return convertView;
         }
