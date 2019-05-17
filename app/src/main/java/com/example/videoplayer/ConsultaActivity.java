289b2e4552381e852ac0c8c97e4cefdbf501790c
JoaoPedroBelo <joao.pedro.v.b@outlook.com>
2019-05-17 17:31:23 +0100

    Fix layout in listview, now listview adapt to the size of a screen

--- app/src/main/java/com/example/videoplayer/ConsultaActivity.java
+++ app/src/main/java/com/example/videoplayer/ConsultaActivity.java
@@ -148,7 +148,7 @@ public class ConsultaActivity extends AppCompatActivity implements OnGesturePerf
      * Called when the user taps the Play movie button or gesture M
      */
     public void playMovie(View view) {
-        Log.d("teste",parseNameYear(title, year));
+        Log.d("Play_File_Name",parseNameYear(title, year));
         Intent intent = new Intent(this, PlayerActivity.class);
         intent.putExtra("movie", parseNameYear(title, year));
         startActivity(intent);
