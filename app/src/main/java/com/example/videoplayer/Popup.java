8fea25ca1a2cec979df96c15785fd9d756d810f4
Diogo Martins <diogo.felix.martins@gmail.com>
2019-05-30 11:10:46 +0100

    shaking the phone in the video player will pause/resume the playback; changed gesture images color;

--- app/src/main/java/com/example/videoplayer/Popup.java
+++ app/src/main/java/com/example/videoplayer/Popup.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
-class Popup extends Activity {
+public class Popup extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
